27fc77561dee41f42ca4d10bb486a344c90aa11b
barc0d2 <yaho73m@gmail.com>
2025-03-27 14:52:26 +0900

    init

---
+++ src/main/java/com/kh/yeast/YeastApplication.java
@@ -0,0 +1,13 @@
+package com.kh.yeast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/yeast/YeastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.yeast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
